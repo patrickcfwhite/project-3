9aa5c4984b30b3af6f33dba69a7a094849e5a60f
annie-kayal <anamikakayal@hotmail.com>
2020-03-29 20:41:44 +0100

    login & token

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bulma'
 import './styles/style.scss'
-// import axios from 'axios'
+import axios from 'axios'
+import './styles/book.scss'
 
 // libraries
 // import Panzoom from 'panzoom'
@@ -34,6 +35,7 @@ const App = () => (
       <Route exact path='/cook' component={Cook}></Route>
       <Route exact path='/watch' component={Watch}></Route>
       <Route exact path='/game' component={Game}></Route>
+      <Route exact path='/read' component={Read}></Route>
       <Route exact path='/register' component={Register}></Route>
       {/* <Route exact path='/login' component={LoginModal}></Route> */}
       <Route path='/user/:id' component={Profile}></Route>
@@ -42,6 +44,37 @@ const App = () => (
     </Switch>
   </Router>
 )
+
+class Read extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/read')
+      .then(res => {
+        this.setState({ books: res.data })
+        console.log(this.state.books)
+      })
+  }
+
+  render() {
+    return <div>
+      <div className="book-container">
+        {this.state.books.map(book => {
+          return <div key={book._id} className='book'>
+            <img src={book.image} />
+            <h2>{book.title}</h2>
+          </div>
+        })}
+
+      </div>
+    </div>
+  }
+}
 
 ReactDOM.render(
   <App />,
